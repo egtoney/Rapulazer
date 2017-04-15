26a59430e31c80b2779e8dbf8bd0ea97ccc79da6
thunderseethe <emsmith1995@gmail.com>
2017-04-15 18:36:51 -0400

    GLSurface successfully renders to black screen, added renderer

--- app/src/main/java/edu/thunderseethe/rapulazer/MainActivity.java
+++ app/src/main/java/edu/thunderseethe/rapulazer/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.thunderseethe.rapulazer;
 
 import android.app.Activity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -11,9 +12,10 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    // Example of a call to a native method
-    //TextView tv = (TextView) findViewById(R.id.sample_text);
-    //tv.setText(stringFromJNI());
+        GLSurfaceView mGLView = (GLSurfaceView)findViewById(R.id.visualizer_view);
+        mGLView.setEGLContextClientVersion(3);
+        mGLView.setPreserveEGLContextOnPause(true);
+        mGLView.setRenderer(new VisualizerGLRenderer());
     }
 
 
---
+++ app/src/main/java/edu/thunderseethe/rapulazer/VisualizerGLRenderer.java
@@ -0,0 +1,28 @@
+package edu.thunderseethe.rapulazer;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by thunderseethe on 4/15/17.
+ */
+
+public class VisualizerGLRenderer implements GLSurfaceView.Renderer {
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+
+    }
+}
