d6d938da5cb91d8d4eb7a51330e4045256b62471
Cody Barnes <crba233@uky.edu>
2017-04-16 08:54:28 -0400

    Yep

--- app/src/main/java/edu/thunderseethe/rapulazer/MainActivity.java
+++ app/src/main/java/edu/thunderseethe/rapulazer/MainActivity.java
@@ -1,14 +1,28 @@
 package edu.thunderseethe.rapulazer;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.database.Cursor;
+import android.media.MediaMetadataRetriever;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
+import android.media.MediaMetadataRetriever;
+import java.util.ArrayList;
+import android.database.Cursor;
+import android.provider.MediaStore;
+import android.net.Uri;
+import android.media.AudioManager;
+
+import java.io.File;
 
 import edu.thunderseethe.rapulazer.AudioLib.AudioFeatures;
 
@@ -80,28 +94,26 @@ public class MainActivity extends Activity {
         mGLView.setEGLContextClientVersion(3);
         mGLView.setPreserveEGLContextOnPause(true);
         mGLView.setRenderer(mRenderer);
-
     }
-
 
     @Override
     protected void onStart() {
         super.onStart();
         if(!mBound) {
             bindService(
-                new Intent(this, AudioVisualizerService.class),
-                mConnection,
-                Context.BIND_AUTO_CREATE
+                    new Intent(this, AudioVisualizerService.class),
+                    mConnection,
+                    Context.BIND_AUTO_CREATE
             );
         }
 
         //Whenever we come back start up again
         mGLView.setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-            | View.SYSTEM_UI_FLAG_FULLSCREEN
-            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         );
     }
 
